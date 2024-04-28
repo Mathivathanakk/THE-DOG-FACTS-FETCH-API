bada9d315a4f2fd7c19201cadf6f909bf1c19243
Mathivathanakk <mathikk06@gmail.com>
2024-04-28 19:54:45 +0530

    completed

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-//fetching the url
+//adding the eventlistener to the button to  display the details
 
-const url = fetch("https://dogapi.dog/api/v2/breeds?page[number]=29");
-url
-  .then((data) => data.json()) //convert to json format and to object
-  .then((result) => {
-    // console.log(result)
-    const objectData = result.data;
-    let tableData = "";
-    //displayed the data in the table
-    objectData.map((values) => {
-      tableData += `<tr>
+const button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+  displayfact();
+});
+
+//fact displayed by fetched api
+function displayfact() {
+  const url = fetch("https://dogapi.dog/api/v2/breeds?page[number]=29");
+  url
+    .then((data) => data.json()) //convert to json format and to object
+    .then((result) => {
+      const objectData = result.data;
+      let tableData = "";
+      //displayed the data in the table
+      objectData.map((values) => {
+        tableData += `<tr>
         <td>${values.type}</td>
         <td>${values.attributes.name}</td>
         <td>${values.attributes.description}</td>
@@ -17,7 +23,9 @@ url
         </tr>
         
         `;
-    });
+      });
 
-    document.querySelector(".tableBody").innerHTML = tableData;
-  }).catch((err)=>console.log(err));//else catch the error
+      document.querySelector(".tableBody").innerHTML = tableData;
+    })
+    .catch((err) => console.log(err));
+}
